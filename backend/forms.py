00e1b2eca34f9c12875f4660a56c70ab484c3406
FND <fnd@localhost.localdomain>
2014-07-10 16:46:26 +0200

    enabled HTML5 client-side validation
    specifying field types and validation attributes allows browsers to
    check common patterns before hitting the network
    
    cf.
    https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Forms/Data_form_validation

--- backend/forms.py
+++ backend/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import Form
 from wtforms import TextField, PasswordField
+from wtforms.fields.html5 import EmailField
+from wtforms.widgets.core import Input, HTMLString
 from wtforms.validators import DataRequired, Email
+
+
+# monkey-patching WTForms `Input` for inferred `required` attribute -- XXX: temporary workaround, hacky and brittle
+_render = Input.__call__
+def render(self, field, **kwargs):
+    html = _render(self, field, **kwargs)
+    if not field.flags.required:
+        return html
+    if not html[-1] == ">":
+        raise RuntimeError("HTML augmentation failed")
+    return HTMLString("%s required>" % html[:-1])
+Input.__call__ = render
 
 
 class SampleForm(Form):
     username = TextField("username")
-    email = TextField("e-mail", validators=[DataRequired(), Email()])
+    email = EmailField("e-mail", validators=[DataRequired(), Email()])
     password = PasswordField("password", validators=[DataRequired()])
